90351b9485eeac79ffc9679c138953d4e3591d46
Didar Yermekuly <yermekulyd@gmail.com>
2024-08-28 13:51:55 +0500

    Initial commit - Add game files

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  "cs-degree": `This is my Software Engineering degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  pc: `This is where I bring ideas to life using JavaScript, TypeScript, and frameworks like React, Vue, and Next.js. 
+    My daily toolkit for crafting dynamic, scalable web applications.`,
+  resume: `Here's my resume, showcasing my journey and achievements in software engineering. 
+    Take a look at my experience and skills. 
+    <a href="https://github.com/yermekulyd/resume/blob/main/Yermekuly_Didar_CV.pdf" target="_blank">Check it out?</a>
+    Contact me at yermekulyd@gmail.com if you have any interesting job opportunities!`,
+  projects: `This portfolio is powered by Kaboom.js, a JavaScript library for game development. 
+    The text you're reading is rendered with HTML/CSS, while the rest of the magic happens in the canvas.`,
+  library: `This library holds the knowledge I've accumulated over the years, from my Bachelor’s degree in Software Engineering to courses in Full Stack Development, Advanced Front-End Technologies, and Algorithms. 
+    It’s a testament to my passion for continuous learning and growth.`,
+  tv: `On this screen, you’ll find glimpses of my world beyond code—strategizing in chess, expressing creativity through drawing, 
+    strumming melodies on my guitar, and carving down snowy slopes on my snowboard. 
+    Each hobby fuels my passion and creativity.`,
+  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+};
